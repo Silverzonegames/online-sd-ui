10cf71b9930fe0dbb8a19756bc0d365aba2688dd
slimerplanet <eppuku@gmail.com>
2023-07-01 20:38:36 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
